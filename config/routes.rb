f32f02cd2aeba7a3ea51ff78ade7d21f6d92abf7
Your Name <you@example.com>
2022-09-28 18:00:24 +0530

    new 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # devise_scope :user do
+  #   delete "/users/sign_out",  :to => "devise/sessions#destroy"
+  # end
+  devise_for :users
+  resources :users
+    mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  root to: "portal#index"
 end
+  
