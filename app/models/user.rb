d5c5040fa28e0c6d1a6c692de8bc7893b50552ff
Your Name <you@example.com>
2022-09-29 14:04:31 +0530

    roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  enum role: [:employer, :applicant]
+  # def employer?
+  #   self.role == "employer"
+  # end
+
+  # def applicant?
+  #   self.role == "applicant"
+  # end
 end
